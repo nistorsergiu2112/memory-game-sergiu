ec9ffbe2acd912ce8a88583e6c8367ea72615ae7
Nistor-Sergiu-Cosmin <nistorsergiu2112@gmail.com>
2018-06-27 16:17:36 +0300

    Final Commit!

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 /*
  * Create a list that holds all of your cards
  */
-const icons = ["fa fa-diamond", "fa fa-diamond", "fa fa-paper-plane-o", "fa fa-paper-plane-o", "fa fa-anchor", "fa fa-anchor", "fa fa-bolt", "fa fa-bolt", "fa fa-cube", "fa fa-cube", "fa fa-leaf", "fa fa-leaf", "fa fa-bicycle", "fa fa-bicycle", "fa fa-bomb", "fa fa-bomb"];
+const iconsList = ["fa fa-diamond", "fa fa-diamond", "fa fa-paper-plane-o", "fa fa-paper-plane-o", "fa fa-anchor", "fa fa-anchor", "fa fa-bolt", "fa fa-bolt", "fa fa-cube", "fa fa-cube", "fa fa-leaf", "fa fa-leaf", "fa fa-bicycle", "fa fa-bicycle", "fa fa-bomb", "fa fa-bomb"];
 const cardsContainer = document.querySelector(".deck");
 
 const secondsContainer = document.querySelector("#seconds");
@@ -16,9 +16,10 @@ let openedCards = [];
 let matchedCards = [];
 
 /*
- * Initialize the game
+ * Initializing the game
  */
 function init() {
+    const icons = shuffle(iconsList);
     for(let i = 0; i < icons.length; i++) {
         const card = document.createElement("li");
         card.classList.add("card");
@@ -44,16 +45,6 @@ function click(card) {
     // Card Click Event
     card.addEventListener("click", function() {
 
-        /*
-        * At the first click, the condition will be true,
-        * and our code below will get executed!
-        *
-        * We will call our `startTimer` function,
-        * Then, set the `isFirstClick` to `false`, so in the next click,
-        * it will be `if(false)` and nothing will happen, as we don't have an `else`!
-        *
-        * THAT'S EXACTLY what we want!
-        */
         if(isFirstClick) {
             // Start our timer
             startTimer();
@@ -64,7 +55,6 @@ function click(card) {
         const currentCard = this;
         const previousCard = openedCards[0];
 
-        // We have an existing OPENED card
         if(openedCards.length === 1) {
 
             card.classList.add("open", "show", "disable");
@@ -104,7 +94,6 @@ function compare(currentCard, previousCard) {
 
     } else {
 
-        // Wait 500ms then, do this!
         setTimeout(function() {
             currentCard.classList.remove("open", "show", "disable");
             previousCard.classList.remove("open", "show", "disable");
@@ -115,7 +104,6 @@ function compare(currentCard, previousCard) {
 
     }
 
-    // Add New Move
     addMove();
 }
 
@@ -123,22 +111,11 @@ function compare(currentCard, previousCard) {
  * Check if the game is over!
  */
 function isOver() {
-    if(matchedCards.length === icons.length) {
+    if(matchedCards.length === iconsList.length) {
 
-        // Stop our timer
         stopTimer();
-
-        /*
-         * Display your popup here, the `alert` is for explanation only!
-         *
-         * In your popup, you should create a button,
-         * To let the user play a new game
-         *
-         * After clicking on that button, you should:
-         *  - Call the `init` function to re-create the cards
-         *  - Call the `reset` function to reset all variables
-         */
-        alert("GAME OVER!");
+        toggleModal();
+        writeModalStats();
 
     }
 }
@@ -163,6 +140,7 @@ function addMove() {
  */
 const starsContainer = document.querySelector(".stars");
 const star = `<li><i class="fa fa-star"></i></li>`;
+
 starsContainer.innerHTML = star + star + star;
 function rating() {
 
@@ -175,37 +153,40 @@ function rating() {
     }
 }
 
-
+/*
+ * Star rating for Modal
+ */
+function getStars() {
+  if( moves < 18) {
+      stars = 3;
+  } else if( moves < 25) {
+      stars = 2;
+  } else {
+      stars = 1;
+  }
+  return stars;
+}
 /*
  * Timer
  */
-//const timerContainer = document.querySelector(".timer");
-//let liveTimer,
+
 totalSeconds = 0;
 
-// Set the default value to the timer's container
-//timerContainer.innerHTML = totalSeconds + 's';
-
-/*
- * We call this function to start our function,
- * the totalSeconds will be increased
- * by 1 after 1000ms (1 second!)
- *
- * HINT: We need to call this function ONCE, and the best time to call it
- * is when the user click on a card (The first card!)
- * This means that our user is start playing now! ;)
- */
  function startTimer() {
     liveTimer = setInterval(function() {
         // Increase the totalSeconds by 1
         totalSeconds++;
 
         calculateTime(totalSeconds);
+
         // Update the HTML Container with the new time
-        //timerContainer.innerHTML = totalSeconds + 's';
+
         secondsContainer.innerHTML = seconds;
         minutesContainer.innerHTML = minutes;
         hoursContainer.innerHTML   = hours;
+
+        // Fixing the 00:00:00 format bug
+
         if (seconds < 10 && minutes < 10) {
           secondsContainer.innerHTML = `0${seconds}`;
           minutesContainer.innerHTML = `0${minutes}`;
@@ -220,14 +201,6 @@ totalSeconds = 0;
     }, 1000);
 }
 
-/*
- * Our timer won't stop. To stop it, we should clearInterval!
- * We will call it when the game is over.
- * So, we will call it at the end of `isOver` function
- *
- * HINT: That's why I created the `liveTimer` variable,
- * to store the setInterval's function, so that we can stop it by its name!
- */
 function stopTimer() {
     clearInterval(liveTimer);
 }
@@ -248,9 +221,7 @@ restartBtn.addEventListener("click", function() {
     // Delete ALL cards
     cardsContainer.innerHTML = "";
 
-    // Call `init` to create new cards
     init();
-
     // Reset the game
     reset();
 
@@ -259,15 +230,49 @@ restartBtn.addEventListener("click", function() {
 /*
  *  Modal
  */
+
+// Show/hide functionality
+
 function toggleModal() {
   const modal = document.querySelector(".modal_background");
   modal.classList.toggle('hide');
 }
 
+// Function that displays modal Stats
+
 function writeModalStats() {
   const timeStat = document.querySelector('.modal_time');
-  const clockTime = document.querySelector()
+  const timeModal = document.querySelector('.timer').innerHTML;
+  const movesStat = document.querySelector('.modal_moves');
+  const starStat = document.querySelector('.modal_stars');
+  const stars = getStars();
+
+  starStat.innerHTML = `Stars = ${stars}`;
+  timeStat.innerHTML = `Time = ${timeModal}`;
+  movesStat.innerHTML = `Moves = ${moves}`;
 }
+
+// Modal buttons event listeners
+const restartBtnModal = document.querySelector('.modal_replay');
+restartBtnModal.addEventListener("click", function() {
+  // Delete ALL cards
+  cardsContainer.innerHTML = "";
+
+  // Call `init` to create new cards
+  init();
+
+  // Reset the game
+  reset();
+  // Hiding Modal
+  toggleModal();
+
+});
+
+const cancelBtnModal = document.querySelector('.modal_cancel');
+cancelBtnModal.addEventListener("click", function() {
+  toggleModal();
+});
+
 /*
  * Reset All Game Variables
  */
@@ -282,15 +287,9 @@ function reset() {
     // Reset `rating`
     starsContainer.innerHTML = star + star + star;
 
-    /*
-     * Reset the `timer`
-     *
-     * - Stop it first
-     * - Then, reset the `isFirstClick` to `true` to be able to start the timer again!
-     * - Don't forget about `totalSeconds`, it must be `0`
-     * - One more thing, is to update the HTML timer's container
-     */
+    // Resetting the timer
     stopTimer();
+
     isFirstClick = true;
     totalSeconds = 0;
     hoursContainer.innerHTML = "00";
@@ -302,34 +301,11 @@ function reset() {
 }
 
 
-/////// Start the game for the first time!
+
+
+// Starting the game
+
 init();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -345,15 +321,3 @@ function shuffle(array) {
 
     return array;
 }
-
-
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
